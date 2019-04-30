3a49693a7fc2dbea5154b504b2808b911081a4c6
Ebuka <ebukaa@protonmail.com>
2019-04-30 23:02:05 +0100

    Added the file path location of the "Realm" storage

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         // Override point for customization after application launch.
+        guard let realmStorageFileURL = Realm.Configuration.defaultConfiguration.fileURL else {
+            return true
+        }
+        print()
+        print(realmStorageFileURL)
+        print()
+        
         return true
     }
 
